496c00216b274f3565e0ec89bc9c2414e92dfe08
Ocean Fuaga <ocean@Oceans-MacBook-Air.local>
2020-08-01 11:38:08 -0700

    Change BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Questions from './Questions';
 import questionsData from '../questionsData';
 import Home from './Home';
